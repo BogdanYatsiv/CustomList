56c65b53604fc2a0a1653dae6bea8d0af96220ba
bogdan_yatsiv <bogdanyatsiv@gmail.com>
2021-08-25 13:45:26 +0300

    Add project files.

---
+++ CustomList/MyList.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class MyList<T> : IList<T>
+    {
+        private T[] arr;
+        private int size;
+        private int capacity;
+
+        public MyList(int initialCapacity = 8)
+        {
+            if (initialCapacity < 1) initialCapacity = 1;
+            this.capacity = initialCapacity;
+            arr = new T[initialCapacity];
+        }
+        public T this[int index] 
+        {
+            get => arr[index]; 
+            set => arr[index] = value; 
+        }
+
+        public int Count => size;
+
+        public bool IsReadOnly => IsReadOnly;
+
+        public void Add(T item)
+        {
+            if(size == capacity)
+            {
+                Resize();
+            }
+
+            arr[size] = item;
+            size++;
+        }
+
+        public void Clear()
+        {
+            arr = new T[capacity];
+            size = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            for(int i = 0; i < size; i++)
+            {
+                T currentVal = arr[i];
+                if (currentVal.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ThrowIfIndexOutOfRange(arrayIndex);
+            int temp = arrayIndex;
+            for(int i = 0; i < size; i++)
+            {
+                array[temp] = arr[i];
+                temp++;
+            }
+        }
+        private IEnumerable<T> GetValues()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                yield return arr[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetValues().GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                T currentVal = arr[i];
+                if (currentVal.Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            ThrowIfIndexOutOfRange(index);
+            if (size == capacity) Resize();
+
+            for(int i = size; i > index; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[index] = item;
+            size++;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                T currentVal = arr[i];
+                if (currentVal.Equals(item))
+                {
+                    arr[i] = default(T);
+                    size--;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ThrowIfIndexOutOfRange(index);
+            arr[index] = default(T);
+            size--;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void Resize()
+        {
+            T[] resized = new T[capacity * 2];
+            for(int i = 0; i < capacity; i++)
+            {
+                resized[i] = arr[i];
+            }
+            arr = resized;
+            capacity *= 2;
+        }
+        private void ThrowIfIndexOutOfRange(int index)
+        {
+            if (index > size - 1 || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(string.Format("The current size of the array is {0}", size));
+            }
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyList<int> l = new MyList<int>();
+
+            l.Add(1);
+            l.Add(2);
+            l.Add(3);
+
+            foreach(var v in l)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
